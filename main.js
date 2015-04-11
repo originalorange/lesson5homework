987e60b9fe63e26f22024e1bea4780c2c2a58d5e
Jen Wilson <originalorange@gmail.com>
2015-04-10 21:52:58 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+	function agecheck(){
+	month = document.getElementById('month').value;
+	day = document.getElementById('day').value;
+	year = document.getElementById('year').value;
+	document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2));
+	}
+
+	function addNums(){
+	num1 = document.getElementById('num1').value;
+	num2 = document.getElementById('num2').value;
+	document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2));
+	}
+
+
+	// Write 3 different functions that take input and return something via the console or an alert.
